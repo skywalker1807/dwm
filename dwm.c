c1c5bf588729111565eaf44bbdde8efe85d72d7e
Kemoyin <50865923+skywalker1807@users.noreply.github.com>
2019-12-03 11:09:54 +0100

    Update dwm.c

--- dwm.c
+++ dwm.c
@@ -1099,14 +1099,20 @@ maprequest(XEvent *e)
 void
 monocle(Monitor *m)
 {
-	unsigned int n = 0;
+	unsigned int a= 0, s= 0;
 	Client *c;
-
+/*
 	for (c = m->clients; c; c = c->next)
 		if (ISVISIBLE(c))
 			n++;
-	if (n > 0) /* override layout symbol */
-		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
+*/	
+	for(c= nexttiled(m->clients), a= 0, s= 0; c; c= nexttiled(c->next), a++)
+		if(c == m->stack)
+			s= a;
+	if(!s && a)
+		s= a;
+	if (n > 0 && a) /* override layout symbol */
+		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d/%d]", s, a);
 	for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
 		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
 }
