c80f0db594afe9a07ead7d5aa155fa39fe177fe5
Kemoyin <50865923+skywalker1807@users.noreply.github.com>
2019-12-11 15:17:34 +0100

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -74,8 +74,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_o,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	/* { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } }, */
